6c02c7b8a6d607478965a946c6bc57762b02a16f
IvanDaRetard <eess25853@gmail.com>
2021-11-09 18:58:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# THE BIO FUNCTION
+def bio(name, born, pronoun):
+        print(name.capitalize(), "was born in", born)
+        age = 2021 - born
+        print(pronoun.capitalize(), "will turn", age, "years old this year")
+        future = born + 67
+        print(pronoun.capitalize(), "will be 67 in the year", future)
+
+# PRINT BIOS
+bio("stephen",1984,"he")
+bio("mary",1990,"she")
+bio("jane",2000,"she")
+
+# EXIT
+quit()
+
+# EXIT
+quit()
+
